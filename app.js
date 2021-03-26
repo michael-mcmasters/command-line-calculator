c306da5487ebe351159118eef82d5a8a881e0142
Michael McMasters <jmcmasters411@gmail.com>
2021-03-26 17:10:33 -0400

    Throw erros and exit program when unexpected input is found

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 #!/usr/bin/env node
-// The above is needed for script to be installed globally on pc and mac.
 
 const input = process.argv.slice(2);
+if (input.length % 2 === 0) {
+  console.error(`Invalid arguments. Expected "2 + 4" or "5 + 2 * 3 / 8"`);
+  process.exit(9);
+}
+// else if length is 1, split it using symbols.
 
+console.log(input);
 let result = parseNumber(input[0]);
 for (let i = 1; i < input.length; i += 2) {
   let symbol = parseSymbol(input[i]);
@@ -16,13 +21,16 @@ console.log(result);
 function parseSymbol(str) {
   if (str === "+" || str === "-" || str === "*" || str === "/" || str === "%")
     return str;
-  throw new error(`Expected a symbol (+, -, *, /, or %), but received ${str}`);
+  console.error(`Expected a symbol (+, -, *, /, or %), but received: ${str}... Note for multiplication (*), you must escape the wildcard symbol as /*`);
+  process.exit(9);
 }
 
 function parseNumber(str) {
   let num = Number(str);
-  if (isNaN(num))
-    throw new error(`Expected a number but received ${num}.`);
+  if (isNaN(num)) {
+    console.error(`Expected a number but received: ${str}`);
+    process.exit(9);
+  }
   return num;
 }
 
@@ -39,62 +47,7 @@ function calculate(num1, symbol, num2) {
     case "%":
       return num1 % num2;
     default:
-      throw new exception("Something went wrong. Unable to calculate.");
+      console.error("Something went wrong. Unable to calculate.");
+      process.exit(9);
   }
 }
-
-
-
-
-
-
-// const input = process.argv.slice(2);
-// let result = input[0];
-// let lookingForSymbol = true;
-// let symbol = "";
-// for (let i = 1; i < input.length; i++) {
-//   if (lookingForSymbol) {
-//     symbol = getSymbol(input[i]);
-//   } else {
-
-//   }
-
-//   console.log(input[i]);
-
-
-//   lookingForSymbol = !lookingForSymbol;
-// }
-
-// function getSymbol(str) {
-
-// }
-
-
-
-
-
-// const [num1, symbol, num2] = parseInput();
-// const output = calculate(num1, symbol, num2);
-// console.log(output);
-
-// function parseInput() {
-//   const input = process.argv.slice(2);
-//   const num1 = Number(input[0]);
-//   const symbol = input[1];
-//   const num2 = Number(input[2]);
-//   return [num1, symbol, num2];
-// }
-
-// function calculate(num1, symbol, num2) {
-//   switch (symbol) {
-//     case "+":
-//       return num1 + num2;
-//     case "-":
-//       return num1 - num2;
-//     case "*":
-//       return num1 * num2;
-//     case "/":
-//       return num1 / num2;
-//     case "%":
-//       return num1 % num2;
-//   }
