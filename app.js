2eed89b28e92255eaffec63315ce1a7109ceb6cf
Michael McMasters <jmcmasters411@gmail.com>
2021-03-26 18:45:35 -0400

    Refactor and comment

--- app.js
+++ app.js
@@ -1,41 +1,47 @@
 #!/usr/bin/env node
 
-const input = getInput();
-
-let result = parseNumber(input[0]);
-for (let i = 1; i < input.length; i += 2) {
-  let symbol = parseSymbol(input[i]);
-  let num = parseNumber(input[i + 1]);
-  result = calculate(result, symbol, num);
-}
+const args = getInput();
+const result = calculateResult(args);
 console.log(result);
 
 
-
+// Get args user passed from the CLI.
 function getInput() {
-  let input = process.argv.slice(2);
-  if (input.length === 1) {
-    // If user didn't put spaces between args: (1+3*2/9)
-    let n = input[0].match(/[0-9]+|\+|\-|\*|\/|\%/g);
-    input = n;
-  } else if (input.length % 2 === 0) {
-    // If there are an even amount of arguments, there is probably a symbol after a number. Can't calculate this: (1 + 2 + ).
+  let argsArr = process.argv.slice(2);
+  if (argsArr.length === 1) {
+    // If user didn't put spaces between args: (1+3*2/9).
+    argsArr = argsArr[0].match(/[0-9]+|\+|\-|\*|\/|\%/g);
+  } else if (argsArr.length % 2 === 0) {
+    // Must be an odd amount of arguments. Can't calculate 4 args such as (1 + 2 + ).
     console.error(`Invalid arguments. The expected format is:  "2 + 4 - 1 \* 3" or "2+4-1*3"`);
     process.exit(9);
   }
-  return input;
+  return argsArr;
+}
+
+// Loop every other index starting at index 1. Get symbol and number after it.
+// 1 (+ 2) (- 3) (* 4) (/ 5).
+function calculateResult(args) {
+  let result = parseNumber(args[0]);
+  for (let i = 1; i < args.length; i += 2) {
+    const symbol = parseSymbol(args[i]);
+    const num = parseNumber(args[i + 1]);
+    result = calculate(result, symbol, num);
+  }
+  return result;
 }
 
 function parseSymbol(str) {
   if (str === "+" || str === "-" || str === "*" || str === "/" || str === "%")
     return str;
+
   console.error(`Expected a symbol (+, -, *, /, or %), but received: ${str}.
   Note for multiplication (*), you must escape the wildcard symbol as /*`);
   process.exit(9);
 }
 
 function parseNumber(str) {
-  let num = Number(str);
+  const num = Number(str);
   if (isNaN(num)) {
     console.error(`Expected a number but received: ${str}`);
     process.exit(9);
